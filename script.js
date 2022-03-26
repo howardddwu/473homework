d2ade5d22a5799988cd1d6486774f7aa6fb47a8c
gspinoza <Espinoza.Gavriell@gmail.com>
2022-03-26 16:22:46 -0400

    added action listener to each button to get value

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+// this returns an array of each button
+var listItems = document.querySelectorAll("section button");
+var displayText = "";
+
+// add action listener to each button
+listItems.forEach(function(item) {
+    item.onclick = function(e) {
+    // get content of clicked button
+    input = this.innerText;
+    // process
+    processUserActions(input);
+    }
+});
